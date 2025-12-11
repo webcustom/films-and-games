f2537711ee6391604e5e7840738cfb7ac4ff19a7
webcustom <webcustom1@gmail.com>
2025-12-11 16:55:02 +0300

    edits

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,7 @@ class HomeController extends Controller
     public function index(Request $request){
         $query = Collection::with(['films', 'games']);
 
+
         $collections = $query->where('published', 1)->whereNotNull('category_id')->latest('published_at')->limit(19)->get();
 
         // $collections = $query->latest('published_at')->where('published', 1)->where('category_id', !null)->paginate(19);
